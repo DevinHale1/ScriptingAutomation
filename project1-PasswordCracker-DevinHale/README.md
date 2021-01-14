3c7bcea4e4d7ebb91b01134aea9293dc748901dd
DevinHale1 <46941199+DevinHale1@users.noreply.github.com>
2021-01-13 23:09:51 -0500

    Add files via upload

---
+++ project1-PasswordCracker-DevinHale/README.md
@@ -0,0 +1,16 @@
+# Project 1z: ROLL YOUR OWN
+
+No actual "rules" here. Just do what you've committed to do. If in doubt, ask!!
+
+## LOGISTICS 
+
+Same basic deal as with the homeworks. 
+
+1. From your AIST2120 folder, run `git clone [URL]` to make a local copy of this assignment. 
+2. DO THE WORK (create and complete any and all needed files)
+3. TEST YOUR WORK A WHOLE LOT 
+4. TEST IT SOME MORE LIKE YOU MEAN IT THIS TIME 
+5. When done, run `git add .` 
+6. Then run `git commit –m "type a clever message here"` 
+7. Finally run `git push` to push your changes back up to GitHub 
+8. Breathe a sigh of relief 
---
+++ project1-PasswordCracker-DevinHale/project1.py
@@ -0,0 +1,110 @@
+#Devin Hale
+#Project 1
+#Dictionary/Brute force attack
+
+import random
+
+
+
+pw = input("Please enter a password for the computer to guess:")
+pwminlength = int(input("Please enter minimum length for password: (cannot be 0) "))
+pwlength = [1,2,3,4,5,6,7,8,9]
+
+
+
+pwlist = list(pw)
+
+pwk = []
+dictpwk = []
+
+index = '1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ !@'
+numindex = '1234567890!@'
+indexl = list(index)
+numindexl = list(numindex)
+
+attempt = 0
+
+choice = "y"
+
+# PY: BE CAREFUL here. It's in the same folder. So while this
+# no doubt worked for you in your environment, it didn't for
+# me when I just tried to run it. Best to just use the "current"
+# directory (as I've done below) or to pass this as a command
+# line parameter.
+# with open("project1z-DevinHale1\password.txt", 'r') as dictionary:
+with open("password.txt", 'r') as dictionary:
+    # PY: I don't see a purpose to this while loop...also causes a problem...see below
+    while attempt < 7092:
+        # PY: Pretty huge bug here. You are reading a line from the file and setting
+        # it to "line" here. Then you are ignoring that one and reading the NEXT line
+        # as dictpass. SOOOO, you are skipping every other password in you file. 
+        for line in dictionary:
+            dictpass = dictionary.readline()
+            dictpwk = list(dictpass.strip())
+            dictlength = len(dictpwk) 
+            addonamount = pwminlength - dictlength # if dictioned word is below minimum password length, adds this many characters
+            attempt += 1
+
+            while addonamount > 0:
+                dictaddon = random.choice(numindexl) 
+                dictpwk.append(dictaddon)
+                addonamount -= 1
+
+            if dictpwk != pwlist:
+                dictpwk = ''.join(dictpwk)
+                dictpwk = dictpwk.capitalize()
+                dictpwk = list(dictpwk)
+                attempt += 1
+
+                if dictpwk != pwlist:
+                    dictpwk =''.join(dictpwk)
+                    dictpwk = dictpwk.lower()
+                    dictpwk = list(dictpwk) 
+                    attempt += 1
+
+                    if dictpwk != pwlist:
+                        dictpwk = ''.join(dictpwk)
+                        dictpwk = dictpwk.upper()
+                        dictpwk = list(dictpwk)
+                        attempt += 1
+                        
+
+            if dictpwk == pwlist:
+                print("Your password: '" + pw +  "' was dictionated in " + str(attempt) + " attempts")
+                break
+                # PY: Another big error here. This break statements exits
+                # the "for" loop. You are then inside of your while attempt < 7092 loop.
+                # This ends up being stuck in an infinite loop because it's ALWAYS < 7092
+
+
+print("Dictionary Attack Failed - Attempting Brute Force...")
+
+# PY: what's the point of this check?? choice is always "y" at this point
+if choice == "y":
+
+    while pwk != pwlist and dictpwk != pwlist:
+        length = random.randrange(pwlength[pwminlength - 1], 10) #will only generate a password between minimum password length and 10
+        while length > 0: 
+            tgpw = random.choice(indexl)  
+            pwk.append(tgpw)
+            length -= 1 
+            
+        if pwlist != pwk:
+            attempt += 1
+            pwk = []
+
+        if attempt == 65000:
+            choice = input("You have attempted to generate this password 65000 times.. Continue forever? (y/n)")
+            if choice != 'y':
+                print("Password generation failure...")
+                break
+
+
+
+if dictpwk != pwlist and pwk == pwlist:     
+    print("Your password: '"+ pw + "' was generated in " + str(attempt) + " attempts")
+
+
+# Grade: 118/125
+# A lot of good stuff in here. A few logic errors and a couple of
+# head-scratchers. But overall a very nice project. Well done!
